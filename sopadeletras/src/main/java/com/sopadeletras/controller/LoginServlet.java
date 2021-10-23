c7b7c01e09df1ef623b80081ba2c31be5c506a54
Moda <Moda@PcEscritorio>
2021-10-23 18:38:18 +0200

    Login con LDAP y Vistas de login

---
+++ sopadeletras/src/main/java/com/sopadeletras/controller/LoginServlet.java
@@ -0,0 +1,90 @@
+package com.sopadeletras.controller;
+
+import java.io.IOException;
+import java.util.Hashtable;
+
+import javax.naming.Context;
+import javax.naming.NamingException;
+import javax.naming.directory.DirContext;
+import javax.naming.directory.InitialDirContext;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    protected void autentificacionLDAP(HttpServletRequest request, HttpServletResponse response)
+    	    throws ServletException, IOException{
+    	    	
+    	    	final String SUCCESS = "acceso.jsp";
+    	    	final String FAILURE = "denegado.jsp";
+    	    	String strUrl = "inicio.jsp";
+    	    	String username = request.getParameter("nombre");
+    	    	String password = request.getParameter("contrasena");
+    	    	
+    	    	Hashtable<String, String> env = new Hashtable<>();
+    	    	
+    	    	//Variable control autenticacion
+    	    	boolean autentificado = false;
+    	    	
+    	    	//Parametros para la conexion LDAP
+    	    	env.put(Context.INITIAL_CONTEXT_FACTORY,"com.sun.jndi.ldap.LdapCtxFactory");
+    	    	env.put(Context.PROVIDER_URL, "ldap://localhost:10389");
+    	    	env.put(Context.SECURITY_AUTHENTICATION, "simple");
+    	    	env.put(Context.SECURITY_PRINCIPAL, "uid=" + username +", ou=users, ou=system");
+    	    	env.put(Context.SECURITY_CREDENTIALS, password);
+    	    	try {
+    	    		//Contexto inicial
+    	    		DirContext ctx = new InitialDirContext(env);
+    	    		
+    	    		//Establecemos variable y cerramos el contexto
+    	    		autentificado = true;
+    	    		ctx.close();
+    	    	} catch (NamingException e) {
+    	    		autentificado = false;
+    	    	}finally {
+    	    		if (autentificado) {
+    	    			System.out.print("Success");
+    	    			strUrl = SUCCESS;
+    	    		}else {
+    	    			System.out.print("Failure");
+    	    			strUrl = FAILURE;
+    	    		}
+    	    	}
+    	    	//eNVIAMOS A jsp CORRESPONDIENTE
+    	    	RequestDispatcher rd = request.getRequestDispatcher(strUrl);
+    	    	rd.forward(request, response);
+    	    }
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
